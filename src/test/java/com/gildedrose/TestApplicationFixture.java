84153267d28f6d7c1bd36db2960bc89582ef500d
james-things <jharr651@students.kennesaw.edu>
2022-02-17 14:18:33 -0500

    Add tests

--- src/test/java/com/gildedrose/TestApplicationFixture.java
+++ src/test/java/com/gildedrose/TestApplicationFixture.java
@@ -5,15 +5,15 @@ public class TestApplicationFixture {
         System.out.println("Test Name Here");
 
         Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("+5 Dexterity Vest", 10, 20),
                 new Item("+3 Strength Gloves", 5, 10),
-                new Item("Aged Brie", 2, 0), //
-                new Item("Aged Brie", 11, 10), //
-                new Item("Aged Brie", 6, 20), //
-                new Item("Aged Brie", 1, 30), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Elixir of the Mongoose", 1, 5), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Aged Brie", 2, 0),
+                new Item("Aged Brie", 11, 10),
+                new Item("Aged Brie", 6, 20),
+                new Item("Aged Brie", 1, 30),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Elixir of the Mongoose", 1, 5),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50),
