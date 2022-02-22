ef64427c21bbe500c40b9a2712416530330d2eea
James <jharr651@students.kennesaw.edu>
2022-02-21 19:36:40 -0500

    Added an example complete test for SetItemFlags(Item)

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -39,16 +39,67 @@ class GildedRoseTest {
 
     @Test
     void setItemFlags() {
-        Item testItem1 = new Item("Conjured Test Item", 10, 10); //declare test item
-        Item testItem2 = new Item("Sulfuras or something", 10, 10);
-        Item[] items = new Item[] { new Item("foo", 0, 0) }; //declare array (needed by gildedrose)
-        GildedRose gr = new GildedRose(items);  //instantiate gildedrose
-        assertFalse(gr.isConjured); //assert default is false
-        gr.SetItemFlags(testItem1); //set flags using first TEST item
-        assertTrue(gr.isConjured); //assert test item changed flag to true
-        gr.SetItemFlags(testItem2); //set flags using second test item
-        assertFalse(gr.isConjured); //assert test item unset previous flag
-        assertTrue(gr.isLegendary); //assert test item set new flag
+        //create a set of test items and instantiate GildedRose class
+        Item testItemC = new Item("Conjured Test Item", 0, 10);
+        Item testItemL = new Item("Sulfuras Test Item", 10, 10);
+        Item testItemA = new Item("Aged Brie Test", 10, 0);
+        Item testItemA2 = new Item("test concert tickets",10, 10);
+        Item testItemN = new Item("Normal Test Item", 10, 10);
+        GildedRose testGildedRose = new GildedRose();
+
+        //assert defaults are false
+        assertFalse(testGildedRose.isAging);
+        assertFalse(testGildedRose.isConjured);
+        assertFalse(testGildedRose.isLegendary);
+        assertFalse(testGildedRose.isNormal);
+        assertFalse(testGildedRose.isPastSellIn);
+        assertFalse(testGildedRose.isUnsellable);
+
+        //test conjured item
+        testGildedRose.SetItemFlags(testItemC);
+        assertFalse(testGildedRose.isAging);
+        assertTrue(testGildedRose.isConjured);
+        assertFalse(testGildedRose.isLegendary);
+        assertFalse(testGildedRose.isNormal);
+        assertTrue(testGildedRose.isPastSellIn);
+        assertFalse(testGildedRose.isUnsellable);
+
+        //test legendary item
+        testGildedRose.SetItemFlags(testItemL);
+        assertFalse(testGildedRose.isAging);
+        assertFalse(testGildedRose.isConjured);
+        assertTrue(testGildedRose.isLegendary);
+        assertFalse(testGildedRose.isNormal);
+        assertFalse(testGildedRose.isPastSellIn);
+        assertFalse(testGildedRose.isUnsellable);
+
+        //test aging item - "Aged Brie"
+        testGildedRose.SetItemFlags(testItemA);
+        assertTrue(testGildedRose.isAging);
+        assertFalse(testGildedRose.isConjured);
+        assertFalse(testGildedRose.isLegendary);
+        assertFalse(testGildedRose.isNormal);
+        assertFalse(testGildedRose.isPastSellIn);
+        assertTrue(testGildedRose.isUnsellable);
+
+        //test conjured item - "concert"
+        testGildedRose.SetItemFlags(testItemA2);
+        assertTrue(testGildedRose.isAging);
+        assertFalse(testGildedRose.isConjured);
+        assertFalse(testGildedRose.isLegendary);
+        assertFalse(testGildedRose.isNormal);
+        assertFalse(testGildedRose.isPastSellIn);
+        assertFalse(testGildedRose.isUnsellable);
+
+        //test normal item
+        testGildedRose.SetItemFlags(testItemN);
+        assertFalse(testGildedRose.isAging);
+        assertFalse(testGildedRose.isConjured);
+        assertFalse(testGildedRose.isLegendary);
+        assertTrue(testGildedRose.isNormal);
+        assertFalse(testGildedRose.isPastSellIn);
+        assertFalse(testGildedRose.isUnsellable);
+
     }
 
     @Test
