09d830bc2678166708f8c0860e83ef081af2662d
james-things <jharr651@students.kennesaw.edu>
2022-02-25 18:11:38 -0500

    Complete solution

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,7 +2,10 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
-    protected boolean isPastSellIn, isUnsellable, isNormal, isAging, isLegendary, isConjured;
+    protected boolean isNormal;
+    protected boolean isAging;
+    protected boolean isLegendary;
+    protected boolean isConjured;
 
     //no arg constructor
     public GildedRose() {}
@@ -13,68 +16,66 @@ class GildedRose {
     }
 
     //set items to passed array
-    protected void SetItems(Item[] items) {
+    protected void setItems(Item[] items) {
         this.items = items;
     }
 
     //processes all items and updates their quality using the appropriate method for the given item type
-    public void UpdateQuality() {
+    public void updateQuality() {
         for (Item item : items) {
-            SetItemFlags(item);
+            setItemFlags(item);
 
             if (this.isAging)
-                UpdateAgingQuality(item);
+                updateAgingQuality(item);
             if (this.isConjured)
-                UpdateConjuredQuality(item);
+                updateConjuredQuality(item);
             if (this.isNormal)
-                UpdateNormalQuality(item);
+                updateNormalQuality(item);
 
-            ApplyQualityLimits(item);
+            applyQualityLimits(item);
 
             if (!this.isLegendary)
-                DecrementSellIn(item);
+                decrementSellIn(item);
         }
     }
 
     //set flags appropriate for the passed item
-    protected void SetItemFlags(Item item) {
+    protected void setItemFlags(Item item) {
         this.isAging = item.name.contains("Aged Brie") || item.name.contains("concert");
         this.isLegendary = item.name.contains("Sulfuras");
         this.isConjured = item.name.contains("Conjured");
         this.isNormal = (!this.isAging && !this.isLegendary && !this.isConjured);
-        this.isPastSellIn = item.sellIn <= 0; //corrected bug, was item.quality
-        this.isUnsellable = item.quality <= 0;
     }
 
     //update the quality of an item which ages
-    protected void UpdateAgingQuality(Item item) {
+    protected void updateAgingQuality(Item item) {
         if (item.sellIn > 10)
             item.quality = item.quality + 1;
         if (item.sellIn <= 10 && item.sellIn > 5)
             item.quality = item.quality + 2;
         if (item.sellIn <= 5 && item.sellIn > 0)
             item.quality = item.quality + 3;
-        if (isPastSellIn)
+        if (item.sellIn <= 0)
             item.quality = 0;
     }
 
     //update the quality of a conjured item
-    protected void UpdateConjuredQuality(Item item) {
-        item.quality -= ((isPastSellIn) ? 4 : 2);
+    protected void updateConjuredQuality(Item item) {
+        item.quality -= ((item.sellIn <= 0) ? 4 : 2);
     }
 
     //update the quality of a normal item
-    protected void UpdateNormalQuality(Item item) {
-        item.quality -= ((isPastSellIn) ? 2 : 1);
+    protected void updateNormalQuality(Item item) {
+        item.quality -= ((item.sellIn <= 0) ? 2 : 1);
     }
 
     //reduce sellIn by 1
-    protected void DecrementSellIn(Item item) {
+    protected void decrementSellIn(Item item) {
         item.sellIn -= 1;
     }
 
     //apply hard quality limits
-    public void ApplyQualityLimits(Item item) {
+    public void applyQualityLimits(Item item) {
         if (this.isLegendary)
             item.quality = 80;
         if (item.quality > 50 && !this.isLegendary)
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,6 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GildedRoseTest {
 
+    GildedRose testGildedRose;
+    GildedRose testGildedRoseNoItems;
+
+    Item testItemC1;
+    Item testItemC2;
+    Item testItemL;
+    Item testItemN1;
+    Item testItemN2;
+    Item testItemA1;
+    Item testItemA2;
+    Item testItemA3;
+    Item testItemA4;
+    Item testItemA5;
+    Item testItemQ1;
+    Item testItemQ2;
+
+    Item[] testItemsSet;
+    Item[] testAgingSet;
+
     //Added a SetItems(Item[]) to make test case writing easier
     //to quickly set the array, you can use the syntax:
     //SetItems(new Item[]{itemA, itemB, itemC});
@@ -16,111 +35,167 @@ class GildedRoseTest {
 
     @BeforeEach
     void setUp() {
+        //create a set of test items and instantiate GildedRose class
+        testItemC1 = new Item("Conjured Test Item", 10, 10);
+        testItemC2 = new Item("Conjured Test Item", 0, 10);
+        testItemL = new Item("Sulfuras Test Item", 10, 10);
+        testItemN1 = new Item("Normal Test Item", 10, 10);
+        testItemN2 = new Item("Normal Test Item", 0, 10);
+        testItemA1 = new Item("Aged Brie Test", 3, 10);
+        testItemA2 = new Item("Aged Brie Test", 7, 10);
+        testItemA3 = new Item("Aged Brie Test", 12, 10);
+        testItemA4 = new Item("Aged Brie Test", 0, 10);
+        testItemA5 = new Item("test concert tickets",7, 10);
+        testItemQ1 = new Item("Too High Quality Test Item", 10, 100);
+        testItemQ2 = new Item("Too Low Quality Test Item", 10, -100);
+
+        testItemsSet = new Item[]{testItemC1,testItemL, testItemN1, testItemA1, testItemA5};
+        testAgingSet = new Item[]{testItemA1, testItemA2, testItemA3, testItemA4};
+
+        testGildedRoseNoItems = new GildedRose();
+        testGildedRose = new GildedRose(testItemsSet);
     }
 
     @AfterEach
     void tearDown() {
+        //reset values for further tests
+        testItemC1 = new Item("Conjured Test Item", 10, 10);
+        testItemC2 = new Item("Conjured Test Item", 0, 10);
+        testItemL = new Item("Sulfuras Test Item", 10, 10);
+        testItemN1 = new Item("Normal Test Item", 10, 10);
+        testItemN2 = new Item("Normal Test Item", 0, 10);
+        testItemA1 = new Item("Aged Brie Test", 3, 10);
+        testItemA2 = new Item("Aged Brie Test", 7, 10);
+        testItemA3 = new Item("Aged Brie Test", 12, 10);
+        testItemA4 = new Item("Aged Brie Test", 0, 10);
+        testItemA5 = new Item("test concert tickets",7, 10);
+        testItemQ1 = new Item("Too High Quality Test Item", 10, 100);
+        testItemQ2 = new Item("Too Low Quality Test Item", 10, -100);
+
+        testItemsSet = new Item[]{testItemC1,testItemL, testItemN1, testItemA1, testItemA5};
+        testAgingSet = new Item[]{testItemA1, testItemA2, testItemA3, testItemA4};
+
+        testGildedRoseNoItems = new GildedRose();
+        testGildedRose = new GildedRose(testItemsSet);
     }
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.UpdateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
 
     @Test
     void setItems() {
-
+        assertNull(testGildedRoseNoItems.items);
+        assertNotNull(testItemsSet[0]);
+        assertNotNull(testItemsSet[1]);
+        assertEquals("Conjured Test Item",testItemsSet[0].name);
+        assertEquals("Sulfuras Test Item",testItemsSet[1].name);
+        testGildedRoseNoItems.setItems(testItemsSet);
+        assertNotNull(testGildedRoseNoItems.items[0]);
+        assertNotNull(testGildedRoseNoItems.items[1]);
+        assertEquals("Conjured Test Item",testGildedRoseNoItems.items[0].name);
+        assertEquals("Sulfuras Test Item",testGildedRoseNoItems.items[1].name);
     }
 
     @Test
     void updateQuality() {
+        for (Item i : testGildedRose.items)
+            assertEquals(10, i.quality);
+        testGildedRose.updateQuality();
+        assertEquals(8, testItemC1.quality);
+        assertEquals(80, testItemL.quality);
+        assertEquals(9, testItemN1.quality);
+        assertEquals(13, testItemA1.quality);
+        assertEquals(12, testItemA5.quality);
     }
 
     @Test
     void setItemFlags() {
-        //create a set of test items and instantiate GildedRose class
-        Item testItemC = new Item("Conjured Test Item", 0, 10);
-        Item testItemL = new Item("Sulfuras Test Item", 10, 10);
-        Item testItemA = new Item("Aged Brie Test", 10, 0);
-        Item testItemA2 = new Item("test concert tickets",10, 10);
-        Item testItemN = new Item("Normal Test Item", 10, 10);
-        GildedRose testGildedRose = new GildedRose();
-
         //assert defaults are false
         assertFalse(testGildedRose.isAging);
         assertFalse(testGildedRose.isConjured);
         assertFalse(testGildedRose.isLegendary);
         assertFalse(testGildedRose.isNormal);
-        assertFalse(testGildedRose.isPastSellIn);
-        assertFalse(testGildedRose.isUnsellable);
 
         //test conjured item
-        testGildedRose.SetItemFlags(testItemC);
+        testGildedRose.setItemFlags(testItemC2);
         assertFalse(testGildedRose.isAging);
         assertTrue(testGildedRose.isConjured);
         assertFalse(testGildedRose.isLegendary);
         assertFalse(testGildedRose.isNormal);
-        assertTrue(testGildedRose.isPastSellIn);
-        assertFalse(testGildedRose.isUnsellable);
 
         //test legendary item
-        testGildedRose.SetItemFlags(testItemL);
+        testGildedRose.setItemFlags(testItemL);
         assertFalse(testGildedRose.isAging);
         assertFalse(testGildedRose.isConjured);
         assertTrue(testGildedRose.isLegendary);
         assertFalse(testGildedRose.isNormal);
-        assertFalse(testGildedRose.isPastSellIn);
-        assertFalse(testGildedRose.isUnsellable);
 
         //test aging item - "Aged Brie"
-        testGildedRose.SetItemFlags(testItemA);
+        testGildedRose.setItemFlags(testItemA1);
         assertTrue(testGildedRose.isAging);
         assertFalse(testGildedRose.isConjured);
         assertFalse(testGildedRose.isLegendary);
         assertFalse(testGildedRose.isNormal);
-        assertFalse(testGildedRose.isPastSellIn);
-        assertTrue(testGildedRose.isUnsellable);
 
         //test aging item - "concert"
-        testGildedRose.SetItemFlags(testItemA2);
+        testGildedRose.setItemFlags(testItemA5);
         assertTrue(testGildedRose.isAging);
         assertFalse(testGildedRose.isConjured);
         assertFalse(testGildedRose.isLegendary);
         assertFalse(testGildedRose.isNormal);
-        assertFalse(testGildedRose.isPastSellIn);
-        assertFalse(testGildedRose.isUnsellable);
 
         //test normal item
-        testGildedRose.SetItemFlags(testItemN);
+        testGildedRose.setItemFlags(testItemN1);
         assertFalse(testGildedRose.isAging);
         assertFalse(testGildedRose.isConjured);
         assertFalse(testGildedRose.isLegendary);
         assertTrue(testGildedRose.isNormal);
-        assertFalse(testGildedRose.isPastSellIn);
-        assertFalse(testGildedRose.isUnsellable);
-
     }
 
     @Test
     void updateAgingQuality() {
+        for (Item i : testAgingSet) {
+            assertEquals(10, i.quality);
+            testGildedRose.updateAgingQuality(i);
+        }
+        assertEquals(13,testAgingSet[0].quality);
+        assertEquals(12,testAgingSet[1].quality);
+        assertEquals(11,testAgingSet[2].quality);
+        assertEquals(0,testAgingSet[3].quality);
     }
 
     @Test
     void updateConjuredQuality() {
+        assertEquals(10, testItemC1.quality);
+        assertEquals(10, testItemC2.quality);
+        testGildedRose.updateConjuredQuality(testItemC1);
+        testGildedRose.updateConjuredQuality(testItemC2);
+        assertEquals(8, testItemC1.quality);
+        assertEquals(6, testItemC2.quality);
     }
 
     @Test
     void updateNormalQuality() {
+        assertEquals(10, testItemN1.quality);
+        assertEquals(10, testItemN2.quality);
+        testGildedRose.updateNormalQuality(testItemN1);
+        testGildedRose.updateNormalQuality(testItemN2);
+        assertEquals(9, testItemN1.quality);
+        assertEquals(8, testItemN2.quality);
     }
 
     @Test
     void decrementSellIn() {
+        assertEquals(10, testItemN1.sellIn);
+        testGildedRose.decrementSellIn(testItemN1);
+        assertEquals(9, testItemN1.sellIn);
     }
 
     @Test
     void applyQualityLimits() {
+        assertEquals(100, testItemQ1.quality);
+        assertEquals(-100, testItemQ2.quality);
+        testGildedRose.applyQualityLimits(testItemQ1);
+        testGildedRose.applyQualityLimits(testItemQ2);
+        assertEquals(50, testItemQ1.quality);
+        assertEquals(0, testItemQ2.quality);
     }
 }
